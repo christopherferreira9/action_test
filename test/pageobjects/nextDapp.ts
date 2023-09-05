d60b866d38dd2f85ca1c43635b2ec5fb7ae11d32
Christopher Ferreira <christopher.ferreira@consensys.net>
2023-09-05 12:26:07 +0100

    Updating poms; reverting node verion on test dockerfile

--- test/pageobjects/nextDapp.ts
+++ test/pageobjects/nextDapp.ts
@@ -1,9 +1,6 @@
 import Page from './page.js'
 
 class NextPage extends Page {
-    /**
-     * define elements
-     */
     get testField () { return $('#testDapp2ID') }
     open () {
         return super.open('http://localhost:3334')
--- test/pageobjects/reactDapp.ts
+++ test/pageobjects/reactDapp.ts
@@ -1,9 +1,6 @@
 import Page from './page.js'
 
 class ReactPage extends Page {
-    /**
-     * define elements
-     */
     get testField () { return $('#testDapp1ID') }
     open () {
         return super.open('http://localhost:3000')
